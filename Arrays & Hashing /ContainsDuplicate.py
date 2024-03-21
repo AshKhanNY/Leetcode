5a0abfdcffd2978580ab97a58dbde395baefa771
Ash Khan <63376512+AshKhanNY@users.noreply.github.com>
2024-03-21 17:07:07 -0400

    Delete Arrays & Hashing directory

--- Arrays & Hashing /ContainsDuplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        tempSet = set()
-        for n in nums:
-            if n in tempSet:
-                return True
-            tempSet.add(n)
-        return False
